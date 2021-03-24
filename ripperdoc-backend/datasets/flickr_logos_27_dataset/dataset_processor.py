0bc52cf71073da8e5c2bd61d18428aebe951d7bd
GiantSweetroll <33752872+GiantSweetroll@users.noreply.github.com>
2021-03-24 15:04:16 +0700

    Now added query dataset trimming

--- ripperdoc-backend/datasets/flickr_logos_27_dataset/dataset_processor.py
+++ ripperdoc-backend/datasets/flickr_logos_27_dataset/dataset_processor.py
@@ -8,17 +8,28 @@ logos_to_find: list = logo_to_find_file.read().splitlines()    # Variable to sto
 print("Logos to find:", logos_to_find)
 
 # Scan through the training dataset and only extract the images that matches the category
-included_data: list = []        # list that stores the matching data
+included_data_train: list = []        # list that stores the matching data
 training_file = open(root_directory + "flickr_logos_27_dataset_training_set_annotation.txt")     #Load the training dataset file
 for x in training_file.read().splitlines():
     for logo in logos_to_find:
         sub:list = x.split()
         if sub[1] == logo:
-            included_data.append(x)
+            included_data_train.append(x)
 
-print(included_data)
+# Scan through the query dataset and only extract the images that matches the category
+included_data_query : list = []
+query_file = open(root_directory + "flickr_logos_27_dataset_query_set_annotation.txt") #Load the query dataset file
+for x in query_file.read().splitlines():
+    for logo in logos_to_find:
+        sub:list = x.split()
+        if sub[1] == logo:
+            included_data_query.append(x)
 
 # Write to file
-trimmed_dataset = open(root_directory + "trimmed_training_dataset.txt", "w")
-for x in included_data:
-    trimmed_dataset.write(x + "\n")
+trimmed_train_dataset = open(root_directory + "trimmed_training_dataset.txt", "w")
+for x in included_data_train:
+    trimmed_train_dataset.write(x + "\n")
+
+trimmed_query_dataset = open(root_directory + "timmed_query_dataset.txt", "w")
+for x in included_data_query:
+    trimmed_query_dataset.write(x + "\n")
