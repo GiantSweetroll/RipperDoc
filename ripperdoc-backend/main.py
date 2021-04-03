75d01a9646e7a757f671519304a9d9fd3126e336
GiantSweetroll <33752872+GiantSweetroll@users.noreply.github.com>
2021-04-03 17:52:12 +0700

    Created initial Flask project

---
+++ ripperdoc-backend/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Home(Resource):
+    def get(self):
+        return {'scan': "These are not the droids you're looking for"}
+
+class APITest(Resource):
+    def get(self):
+        return {'about':"Hello World!"}
+
+    def post(self):
+        some_json = request.get_json()
+        return {'you sent': some_json}, 201
+
+api.add_resource(Home, "/")
+api.add_resource(APITest, '/apitest')
+
+if __name__ == '__main__':
+    app.run(debug=True)
