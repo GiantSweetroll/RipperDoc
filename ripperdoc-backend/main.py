bd13f574f3df4e141dddaf8ec7af4d9c1beec6f2
GiantSweetroll <33752872+GiantSweetroll@users.noreply.github.com>
2021-05-03 15:03:38 +0700

    Made adjustments to swagger path

--- ripperdoc-backend/main.py
+++ ripperdoc-backend/main.py
@@ -2,12 +2,20 @@ from flask import Flask, request
 from flask_restx import Resource, Api
 
 app = Flask(__name__)
-api = Api(app)
+api = Api(app = app,
+            version = "1.0",
+            title = "RipperDoc API Documentation",
+            description = "Upload image of logo to be processed and identified.",
+            doc = "/docs/")
 
+name_space = api.namespace('RipperDoc', description='Manage RipperDoc API')
+
+@name_space.route("/")
 class Home(Resource):
     def get(self):
         return {'scan': "These are not the droids you're looking for"}
 
+@name_space.route("/apitest")
 class APITest(Resource):
     def get(self):
         return {'about':"Hello World!"}
@@ -15,9 +23,6 @@ class APITest(Resource):
     def post(self):
         some_json = request.get_json()
         return {'you sent': some_json}, 201
-
-api.add_resource(Home, "/")
-api.add_resource(APITest, '/apitest')
 
 if __name__ == '__main__':
     app.run(debug=True)
