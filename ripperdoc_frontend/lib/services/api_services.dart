5117c814042c9649fbf7c97f79436799198587fa
GiantSweetroll <33752872+GiantSweetroll@users.noreply.github.com>
2021-05-09 11:18:51 +0700

    Added POST and GET functions to send image and receive ai result (untested(

---
+++ ripperdoc_frontend/lib/services/api_services.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:ripperdoc_frontend/shared/constants.dart';
+
+/// POST image into backend server
+///
+/// userID: the user id from firebase
+/// base64String: the image bytes string encoded in Base64 format
+Future<http.Response> postImage(String userID, String base64String) {
+  return http.post(
+    Uri.https(domainName, "$backendSubdomain/$userID"),
+    headers: <String, String> {
+      'Content-Type' : 'application/json; charset=UTF-8'
+    },
+    body: jsonEncode(<String, String> {
+      "id" : userID,
+      "image" : base64String,
+    }),
+  );
+}
+
+/// GET the identified logo served from backend server.
+///
+/// userID; the user id from firebase
+Future<http.Response> getLogoLabel(String userID) {
+  return http.get(Uri.https(domainName, "$backendSubdomain/$userID"));
+}
---
+++ ripperdoc_frontend/lib/shared/constants.dart
@@ -0,0 +1,2 @@
+final String domainName = "127.0.0.0";
+final String backendSubdomain = "api-backend";
