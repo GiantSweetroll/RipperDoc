ff6a07208bc17b3cd95047991e0b46a27d23c4b4
GiantSweetroll <33752872+GiantSweetroll@users.noreply.github.com>
2021-05-09 15:19:42 +0700

    Added method to normalize dataset

--- ripperdoc-backend/methods.py
+++ ripperdoc-backend/methods.py
@@ -106,3 +106,9 @@ def show_prediction_graph_with_label(image, prediction:str, label:str):
     plt.title("Prediction: " + prediction + " Label: " + label)
     plt.imshow(image, cmap="gray")
     plt.show()
+
+def normalize_dataset(ds, normalization_layer = None):
+    """ Normalize the dataset """
+    if normalization_layer == None:
+        normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
+    return ds.map(lambda x, y: (normalization_layer(x), y))
