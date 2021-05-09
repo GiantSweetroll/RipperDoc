4f01c6ed2e7bf76dd98bcd575c987536d3df8162
GiantSweetroll <33752872+GiantSweetroll@users.noreply.github.com>
2021-05-09 12:26:24 +0700

    Filtered flickr27 dataset

--- ripperdoc-backend/constants.py
+++ ripperdoc-backend/constants.py
@@ -8,9 +8,9 @@ labels:[] = ["Apple", "HP", "Google", "Intel"]
 
 # Flickr 27 dataset
 flickr_27_dataset_folder = 'datasets/flickr_logos_27_dataset/'
-flickr_27_images_folder = flickr_27_dataset_folder + 'flickr_logos_27_dataset_images/'
+flickr_27_images_folder = flickr_27_dataset_folder + 'ripperdoc_filtered/'
 flickr_27_training_dataset_file = flickr_27_dataset_folder + 'trimmed_training_dataset.txt'
-flickr_27_test_dataset_file = flickr_27_dataset_folder + 'timmed_query_dataset.txt'
+flickr_27_validation_dataset_file = flickr_27_dataset_folder + 'trimmed_validation_dataset.txt'
 
 class Dataset(enum.Enum):
     Flickr27 = 1
--- ripperdoc-backend/datasets/flickr_logos_27_dataset/dataset_processor.py
+++ ripperdoc-backend/datasets/flickr_logos_27_dataset/dataset_processor.py
@@ -1,6 +1,8 @@
-import os as os
+import shutil
 
 root_directory : str = "datasets/flickr_logos_27_dataset/"      # The root directory of where to get the files
+images_source_directory: str = root_directory + 'flickr_logos_27_dataset_images/'    # Location of the original images
+filtered_images_directory: str = root_directory + 'ripperdoc_filtered/'         # Location of filetered images
 
 # Process what category to find
 logo_to_find_file = open(root_directory + "logo_to_find.txt", "r")    # load the file that contains what logo to find from the training datasets
@@ -15,21 +17,23 @@ for x in training_file.read().splitlines():
         sub:list = x.split()
         if sub[1] == logo:
             included_data_train.append(x)
+            shutil.copy2(images_source_directory + sub[0], filtered_images_directory + sub[0])      # Copy image to new directory
 
 # Scan through the query dataset and only extract the images that matches the category
-included_data_query : list = []
+included_data_validation : list = []
 query_file = open(root_directory + "flickr_logos_27_dataset_query_set_annotation.txt") #Load the query dataset file
 for x in query_file.read().splitlines():
     for logo in logos_to_find:
         sub:list = x.split()
         if sub[1] == logo:
-            included_data_query.append(x)
+            included_data_validation.append(x)
+            shutil.copy2(images_source_directory + sub[0], filtered_images_directory + sub[0])      # Copy image to new directory
 
 # Write to file
 trimmed_train_dataset = open(root_directory + "trimmed_training_dataset.txt", "w")
 for x in included_data_train:
     trimmed_train_dataset.write(x + "\n")
 
-trimmed_query_dataset = open(root_directory + "timmed_query_dataset.txt", "w")
-for x in included_data_query:
+trimmed_query_dataset = open(root_directory + "trimmed_validation_dataset.txt", "w")
+for x in included_data_validation:
     trimmed_query_dataset.write(x + "\n")
