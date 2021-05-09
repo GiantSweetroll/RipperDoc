d7ba1823053079f157ade4acc7192ca33bc84a38
GiantSweetroll <33752872+GiantSweetroll@users.noreply.github.com>
2021-05-09 15:19:28 +0700

    Changed flickr27 image dataset location

--- ripperdoc-backend/constants.py
+++ ripperdoc-backend/constants.py
@@ -3,12 +3,14 @@ import enum
 image_width = 299
 image_height = 299
 color_channels = 3
+default_batch_size = 16
 
 labels:[] = ["Apple", "HP", "Google", "Intel"]
 
 # Flickr 27 dataset
 flickr_27_dataset_folder = 'datasets/flickr_logos_27_dataset/'
 flickr_27_images_folder = flickr_27_dataset_folder + 'ripperdoc_filtered/'
+# flickr_27_images_folder = flickr_27_dataset_folder + 'flickr_logos_27_dataset_images/'
 flickr_27_training_dataset_file = flickr_27_dataset_folder + 'trimmed_training_dataset.txt'
 flickr_27_validation_dataset_file = flickr_27_dataset_folder + 'trimmed_validation_dataset.txt'
 
