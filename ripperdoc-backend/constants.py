be40a758f86a0d4207685225933e0909f7131a70
GiantSweetroll <33752872+GiantSweetroll@users.noreply.github.com>
2021-05-11 09:39:59 +0700

    Added path for model location

--- ripperdoc-backend/constants.py
+++ ripperdoc-backend/constants.py
@@ -1,9 +1,12 @@
 import os
 
-image_width = 299
-image_height = 299
+
+image_width = 224
+image_height = 224
 color_channels = 3
 default_batch_size = 16
 
-dataset_loc:str = "L:/For Machine Learning/Project/RipperDoc/dataset"
+dataset_loc:str = "L:/For Machine Learning/Project/RipperDoc/dataset/"
+model_loc:str = "L:/For Machine Learning/Project/RipperDoc/models/"
+
 labels:[] = os.listdir(dataset_loc)        # The classifications
