1bab28d6242eecbe8ea2d8ead2a1eedfdf9d8fe8
GiantSweetroll <33752872+GiantSweetroll@users.noreply.github.com>
2021-05-11 17:10:57 +0700

    Cleaned up code

--- ripperdoc-backend/train_ai.py
+++ ripperdoc-backend/train_ai.py
@@ -3,29 +3,13 @@ from neural_network import NeuralNetwork
 import file_operations as io
 import tensorflow as tf
 import os
+import constants
+import file_operations
 
 # Load AI
 print('Loading AI...')
-ai = NeuralNetwork(model=io.load_model('ai/xception_hn'))
+ai = NeuralNetwork(model=io.load_model(constants.model_loc + 'logo_recog_65'))
 print('AI loaded successfully')
 
-ai.train(batch_size=16, epochs=50)
-
-# # Preparing for Tensorflow Serving
-# model_directory = 'ai/'
-# print("Model directory:", model_directory)
-
-# version = 1
-
-# export_path = os.path.join(model_directory, str(version))
-# print('export_path = {}\n'.format(export_path))
-
-# tf.keras.models.save_model(
-#     ai.get_model(),
-#     export_path,
-#     overwrite=True,
-#     include_optimizer=True,
-#     save_format=None,
-#     signatures=None,
-#     options=None
-# )
+ai.train(batch_size=64, epochs=50)
+ai.save(constants.model_loc + ai.get_model().name)
