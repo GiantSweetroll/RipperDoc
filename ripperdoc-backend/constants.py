147c4dab08169485795820b06696da275dfb7fc6
GiantSweetroll <33752872+GiantSweetroll@users.noreply.github.com>
2021-05-11 09:52:50 +0700

    Changed how labels are listed

--- ripperdoc-backend/constants.py
+++ ripperdoc-backend/constants.py
@@ -1,6 +1,3 @@
-import os
-
-
 image_width = 224
 image_height = 224
 color_channels = 3
@@ -9,4 +6,8 @@ default_batch_size = 16
 dataset_loc:str = "L:/For Machine Learning/Project/RipperDoc/dataset/"
 model_loc:str = "L:/For Machine Learning/Project/RipperDoc/models/"
 
-labels:[] = os.listdir(dataset_loc)        # The classifications
+labels:[] = [
+    'Alienware',
+    'Apple',
+    'HP'
+]       # The classifications
