4c89dfc9988d0e9dec0175374a6255e400403571
GiantSweetroll <gardyanakbar99@gmail.com>
2021-05-14 12:00:41 +0700

    Changed domainName and added port for backend

--- ripperdoc_frontend/lib/shared/constants.dart
+++ ripperdoc_frontend/lib/shared/constants.dart
@@ -1,7 +1,9 @@
 import "package:flutter/material.dart";
+import 'package:flutter/foundation.dart' show kIsWeb;
 
-final String domainName = "localhost:5000";
-final String backendSubdomain = "api-backend";
+const String domainName = kIsWeb ? "localhost" : "10.0.2.2";
+const String backendSubdomain = "api-backend";
+const int backendPort = 5000;
 
 const Color colorAppBase = Color.fromRGBO(203, 222, 192, 1.0);
 const Color colorAppSecondary = Color.fromRGBO(203, 250, 192, 1.0);
